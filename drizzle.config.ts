3a34acacb6356be8792d4ad5df73684d8b2a9e26
Aurelio Aquino <aurelio.obmep@gmail.com>
2025-03-03 12:19:52 -0400

    feat: database

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,12 +1,18 @@
-import { type Config } from "drizzle-kit";
+// drizzle.config.ts
 
+import { type Config } from "drizzle-kit";
 import { env } from "~/env";
 
 export default {
-  schema: "./src/server/db/schema.ts",
-  dialect: "sqlite",
-  dbCredentials: {
-    url: env.DATABASE_URL,
-  },
-  tablesFilter: ["google-drive-clone_*"],
+    schema: "./src/server/db/schema.ts",
+    dialect: "singlestore",
+    dbCredentials: {
+        host: env.SINGLESTORE_HOST,
+        port: parseInt(env.SINGLESTORE_PORT),
+        user: env.SINGLESTORE_USR,
+        password: env.SINGLESTORE_PW,
+        database: env.SINGLESTORE_DB,
+        ssl: {}
+    },
+    tablesFilter: ['drive-tutorial_*'],
 } satisfies Config;
