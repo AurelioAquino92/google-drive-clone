be50bf8598857e650beda49febfbdc9315e7cd51
Aurelio Aquino <aurelio.obmep@gmail.com>
2025-03-05 22:27:04 -0400

    feat: db schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,9 +1,35 @@
-import { int, singlestoreTableCreator, text } from "drizzle-orm/singlestore-core";
+import { bigint, index, int, singlestoreTableCreator, text } from "drizzle-orm/singlestore-core";
 
 export const createTable = singlestoreTableCreator((name) => `drive_tutorial_${name}`)
 
 export const users = createTable("users", {
-  id: int("id").primaryKey().autoincrement(),
+  id: bigint({ mode: "number", unsigned: true }).primaryKey().autoincrement(),
   name: text("name"),
   age: int("age")
 })
+
+export const files = createTable(
+  "files", 
+  {
+    id: bigint({ mode: "number", unsigned: true }).primaryKey().autoincrement(),
+    name: text("name").notNull(),
+    size: int("size").notNull(),
+    url: text("url").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true }).notNull()
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)]
+  }
+)
+
+export const folders = createTable(
+  "folders", 
+  {
+    id: bigint({ mode: "number", unsigned: true }).primaryKey().autoincrement(),
+    name: text("name").notNull(),
+    parent: bigint("parent", { mode: "number", unsigned: true })
+  },
+  (t) => {
+    return [index("parent_index").on(t.parent)]
+  }
+)
