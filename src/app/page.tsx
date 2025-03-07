61b6b7c9edc90d8f76e0af02cc9e662ea2c768dc
Aurelio Aquino <aurelio.obmep@gmail.com>
2025-03-06 20:51:01 -0400

    folder ids are handled via the url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import { db } from "~/server/db";
 import DriveContents from "./drive-contents";
 import { files as filesSchema, folders as foldersSchema } from "~/server/db/schema";
 
-export default async function Home() {
+export default async function HomePage() {
 
   const folders = await db.select().from(foldersSchema)
   const files = await db.select().from(filesSchema)
